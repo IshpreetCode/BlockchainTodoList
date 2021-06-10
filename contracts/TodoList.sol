57b807ee0a56f9572f561222d6f6af9aa8157500
IshpreetCode <ishpreetcode@gmail.com>
2021-06-10 18:41:32 +0530

    Tutorial completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,41 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event CompleteToggled(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+    createTask("Default Task");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit CompleteToggled(_id, _task.completed);
+  }
+
 }
